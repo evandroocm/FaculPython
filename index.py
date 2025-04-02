5305b40a6bb079b4b435d18f593eb4a7faf4fffc
Evandro Marques <evandrocastromarques@gmail.com>
2025-04-02 19:29:19 -0400

    commit

--- index.py
+++ index.py
@@ -10,3 +10,17 @@ print(a[1:4])
 
 lista1 = [["asdf"], 34, 5.4, [], ["batata"], False]
 print(len(lista1))
+
+#del
+
+#append
+
+#sort a.sort
+
+#reverse 
+
+#index a.index(4)
+
+#insert     insert(indice, elemento)       a.insert(1, 100)
+
+#pop    a.pop exclui o ultimo elemento
